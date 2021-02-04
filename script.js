9a4f78d670fcaf5dae1efed29aadf6934ab14ec7
ElephantMal <76851937+ElephantMal@users.noreply.github.com>
2021-02-04 09:48:11 -0500

    commit 2

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function openGift(evt, whatGet) {
+  var i, tabcontent, tablinks;
+  tabcontent = 
+document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className =
+tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(whatGet).style.display = 
+    "block";
+    evt.currentTarget.className += " active";
+}
